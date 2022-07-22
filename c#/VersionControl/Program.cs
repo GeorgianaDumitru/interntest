83b2d339a0081a301f9a514fb347d810fef87363
GeorgianaDumitru <56822415+GeorgianaDumitru@users.noreply.github.com>
2022-07-22 01:36:36 -0700

    added c# implementation

---
+++ c#/VersionControl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class VersionControl
+{
+    static void Main()
+    {        
+        Console.WriteLine("Enter Version");
+         
+        string name;
+        name = Console.ReadLine();
+
+        string[] operands = Regex.Split(name, @"\.");
+
+            Console.WriteLine("Major:" + operands[0]);
+            Console.WriteLine("Minor:" + operands[1]);
+            Console.WriteLine("Bugfix:" + operands[2]);
+        
+    }
+}
---
+++ c#/xml/xml.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Xml;
+
+XmlDocument xml = new XmlDocument();
+xml.Load("https://rss.nytimes.com/services/xml/rss/nyt/World.xml");
+
+XmlNodeList xnList = xml.SelectNodes("rss/channel/item");
+foreach (XmlNode xn in xnList)
+{
+    try
+    { 
+
+    string title = xn["title"].InnerText;
+    string creator = xn["dc:creator"].InnerText;
+    string description = xn["description"].InnerText;
+    string date = xn["pubDate"].InnerText;
+
+    Console.WriteLine("Title:" + title);
+    Console.WriteLine("Creator:" + creator);
+    Console.WriteLine("Description:" + description);
+    Console.WriteLine("Date:" + date);
+    Console.WriteLine(" ");
+}
+    catch (NullReferenceException e)
+{
+
+}
+}
+
